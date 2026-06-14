ff55cbbf033f86e2a927eba5efe5b05427132ab6
md-jahirul-islam-tuku <tukuwebian@gmail.com>
2026-06-14 08:00:49 +0300

    query-5 created

--- ticket_booking_query.sql
+++ ticket_booking_query.sql
@@ -165,3 +165,13 @@ FROM
     bookings b
     INNER JOIN users u ON b.user_id = u.user_id
     INNER JOIN matches m ON b.match_id = m.match_id;
+
+SELECT
+    u.user_id,
+    full_name,
+    booking_id
+FROM
+    users u
+    LEFT JOIN bookings b ON u.user_id = b.user_id;
+
+
