3631c9499ffc533ce298ab08aa0279ac024a99d5
md-jahirul-islam-tuku <tukuwebian@gmail.com>
2026-06-14 08:02:46 +0300

    query-6 created

--- ticket_booking_query.sql
+++ ticket_booking_query.sql
@@ -174,4 +174,18 @@ FROM
     users u
     LEFT JOIN bookings b ON u.user_id = b.user_id;
 
+SELECT
+    booking_id,
+    match_id,
+    total_cost
+FROM
+    bookings
+WHERE
+    total_cost > (
+        SELECT
+            avg(total_cost)
+        FROM
+            bookings
+    );
+
 
